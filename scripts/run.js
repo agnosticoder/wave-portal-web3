cd04c136387b4e050e3a7e253010b9987dc7b0de
Satinder Singh <agnosticoder@gmail.com>
2022-02-23 20:36:52 -0500

    initial Commit

---
+++ scripts/run.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+const main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+    console.log('Contract deployed to:', waveContract.address);
+}
+
+const runMain = async () => {
+    try{
+        await main();
+        process.exit(0);
+    }catch(err){
+        console.error({err});
+        process.exit(1);
+    }
+}
+
+runMain();
+
