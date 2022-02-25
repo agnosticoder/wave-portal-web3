a52097b9780a5102c4d6361fc40c588a4694cb56
Satinder Singh <agnosticoder@gmail.com>
2022-02-24 19:20:31 -0500

    Convert to typescript

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal{
+
+    uint256 waveCount;
+
     constructor(){
         console.log('Pal, are you ready to learn web3, are you that guy?');
+    }
+
+    function wave() public{
+        waveCount = waveCount + 1;
+        console.log('%s has waved', msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256){
+        console.log('we have %d total waves', waveCount);
+        return waveCount;
     }
 }
